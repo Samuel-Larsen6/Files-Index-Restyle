d01a9a9eb7f6bdfc21663425b84f2815022b6780
Samuel-Larsen6 <95050709+Samuel-Larsen6@users.noreply.github.com>
2021-11-25 14:55:31 -0600

    Delete ThemeApp directory

--- ThemeApp/Light.js
+++
@@ -1,3 +0,0 @@
-var theme=document.createElement("STYLE");
-theme.innerText="@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'); html { font-family: 'Open Sans', sans-serif; }";
-document.head.appendChild(theme);
